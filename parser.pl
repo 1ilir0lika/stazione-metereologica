346da9bf77e7c30b873d1baa18a2c53cc606ebf9
ilir lika <49962713+1ilir0lika@users.noreply.github.com>
2022-08-24 18:34:02 +0200

    Update parser.pl

--- parser.pl
+++ parser.pl
@@ -1,23 +1,10 @@
 use List::MoreUtils qw( minmax );
 use strict;
-my $j=0;
-my @giorni;
 my @keys;
 my $line;
 my @values;
-my $i;
-my $usum;
-my @umid;
-my $tsum;
-my $mint;
-my @temp;
-my $maxt;
-my $minum;
-my $maxum;
-my $maxt;
-my @temp;
-open(fh,"<","result.json") or die $!;
-while($line = <fh>) {
+open(my $fh,"<","result.json") or die $!;
+while($line = <$fh>) {
   if($line =~ /"date"/){
     push(@keys,$line);
 }
@@ -28,7 +15,20 @@ while($line = <fh>) {
     push(@values,substr($line,22,7));
 }
 }
-close(fh);
+close($fh);
+my $minum;
+my $maxum;
+my $maxt;
+my @temp;
+my $maxt;
+my @umid;
+my $tsum;
+my $mint;
+my @temp;
+my $usum;
+my $i;
+my @giorni;
+my $j=0;
 for (@keys){
   #guarda solo il giorno,poi dovrei aggiustarlo e fargli guardare anche il mese e l'anno
   if(substr($_,20,2) ne  substr($giorni[$#giorni],20,2) ){
@@ -45,17 +45,18 @@ for (@keys){
     push(@temp,substr($values[$i],0,-1));
 	$tsum+=$values[$i];    
 }}
-
     elsif(substr($keys[$i],20,2) ne substr(@giorni[$j],20,2)){
+    open(my $wh,">>","dati.tsv") or die $!;
     ($mint, $maxt) = minmax @temp;
     ($minum, $maxum) = minmax @umid;
-    print(substr(@giorni[$j],12,10)." : \n");
-    print("MIN temp  = ".$mint."C \n");
-    print("MAX temp  = ".$maxt."C \n");
-    print("MEAN temp  = ".substr($tsum/scalar @temp,0,5)."C \n");
-    print("MIN um  = ".$minum."% \n");
-    print("MAX um = ".$maxum."% \n");
-    print("MEAN um  = ".substr($usum/scalar @umid,0,5)."% \n");
+    print($wh substr(@giorni[$j],12,10)." : \n");
+    print($wh "MIN temp  = ".$mint."C \n");
+    print($wh "MAX temp  = ".$maxt."C \n");
+    print($wh "MEAN temp  = ".substr($tsum/scalar @temp,0,5)."C \n");
+    print($wh "MIN um  = ".$minum."% \n");
+    print($wh "MAX um = ".$maxum."% \n");
+    print($wh "MEAN um  = ".substr($usum/scalar @umid,0,5)."% \n");
+    close($wh);
     $j++;
       }
   }
