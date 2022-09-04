5c04492c9640d9fc489190542b95c0f301b2c466
ilir lika <49962713+1ilir0lika@users.noreply.github.com>
2022-09-04 18:05:43 +0200

    Update isto.R

--- grafici/isto.R
+++ grafici/isto.R
@@ -1,9 +1,9 @@
 pdf("istogramma.pdf")
 library(ggplot2)
-df <- data.frame(giorno=c('A', 'B', 'C', 'D', 'E'),
-	         valore=c(12, 17, 30, 22, 19),
-	         err=c(4, 5, 7, 4, 2))
+df <- data.frame(giorno=c('2022-08-22', '2022-08-23', '2022-08-24', '2022-08-25', '2022-08-26','2022-08-27','2022-08-28','2022-08-29','2022-08-30','2022-08-31','2022-09-01','2022-09-02','2022-09-03','2022-09-04','2022-09-05'),
+	         valore=1:15,
+	         err=rep(1,15))
 ggplot(df) +
-	    geom_bar(aes(x=giorno, y=valore), stat='identity', fill='steelblue') +
-	        geom_errorbar(aes(x=giorno, ymin=valore-err, ymax=valore+err), width=0.4)
+	    geom_bar(aes(x=as.Date.character(giorno), y=valore),stat='identity', fill='steelblue') +
+	        geom_errorbar(aes(x=as.Date.character(giorno), ymin=valore-err, ymax=valore+err), width=0.4)
 	dev.off()
